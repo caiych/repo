b5506c6850867aa7b41c1abef8c011354fce94a8
Caiych <caiych@gmail.com>
2021-06-19 04:25:19 +0000

    Move taskfile to dev deps and use dedicated task for CI

--- .gitpod/setup-build.sh
+++ .gitpod/setup-build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 go get github.com/bazelbuild/bazelisk
-go get github.com/go-task/task/v3/cmd/task
--- .gitpod/setup-dev.sh
+++ .gitpod/setup-dev.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 go get github.com/bazelbuild/buildtools/buildifier
+go install github.com/go-task/task/v3/cmd/task@latest
