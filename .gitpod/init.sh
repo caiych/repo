09edaf4600f3a88433170feff17e850b1833efa4
Caiych <caiych@gmail.com>
2021-06-19 01:22:57 +0000

    Use taskfile in init.sh

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 go get github.com/bazelbuild/bazelisk
-
-bazelisk test //...
-
-bazelisk build //:gazelle
-
 go install github.com/go-task/task/v3/cmd/task@latest
+task testall
+task glaze
