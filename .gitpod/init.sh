b0cac88da01066eddaffa000d4f8717f1c6aefee
Caiych <caiych@gmail.com>
2021-06-19 02:33:25 +0000

    Update init script

--- .gitpod/init.sh
+++ .gitpod/init.sh
@@ -2,11 +2,13 @@
 
 # go get github.com/bazelbuild/bazelisk
 
-sudo apt install apt-transport-https curl gnupg
+yes | sudo apt install apt-transport-https curl gnupg
 curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor > bazel.gpg
 sudo mv bazel.gpg /etc/apt/trusted.gpg.d/
 echo "deb [arch=amd64] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 sudo apt update && sudo apt install bazel
+
+go get github.com/bazelbuild/buildtools/buildifier
 
 
 go install github.com/go-task/task/v3/cmd/task@latest
