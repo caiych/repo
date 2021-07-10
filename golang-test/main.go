5e857a035868cad14066cea859f222f0f122eb1f
Caiych <caiych@gmail.com>
2021-07-10 02:22:54 +0000

    Add a proto lib

--- golang-test/main.go
+++ golang-test/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"flag"
 
+	pb "github.com/example/project/golang-test/proto"
 	"github.com/golang/glog"
 )
 
 func main() {
 	flag.Parse()
 	glog.Infof("Hi all!")
+
+	r := &pb.Req{
+		Id: "1",
+	}
+
+	glog.Infof("%v", r)
 }
