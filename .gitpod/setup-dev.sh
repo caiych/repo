bcf837d305f374e83a9f82caf81347768882eb33
Caiych <caiych@gmail.com>
2021-07-10 11:06:26 +0800

    Configure output_base. (#6)
    https://community.gitpod.io/t/external-caches-in-prebuilds/2400

--- .gitpod/setup-dev.sh
+++ .gitpod/setup-dev.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 go install github.com/bazelbuild/buildtools/buildifier@latest
 go install github.com/go-task/task/v3/cmd/task@latest
+
+echo "startup --output_base=/workspace/.bazel-cache" > /workspace/repo/.bazelrc
